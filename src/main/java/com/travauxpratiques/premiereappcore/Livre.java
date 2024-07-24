8a67075f4ec67266f253eba290fdc818ddf3b514
WANG <wangqianlf@live.com>
2024-07-24 17:55:47 +0200

    création d'une Class Livre avec le paramètre numeroLivre en respectant le format JavaBean

---
+++ src/main/java/com/travauxpratiques/premiereappcore/Livre.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.travauxpratiques.premiereappcore;
+
+/**
+ *
+ * @author wangq
+ */
+public class Livre {
+    //On va écrire cette Class au format JavaBean
+    private int numeroLivre;
+    //On va ajouter un getter(accesseur) et un setter (mutateur)
+    public int getNumeroLivre() {
+        return numeroLivre;
+    }
+
+    public void setNumeroLivre(int numeroLivre) {
+        this.numeroLivre = numeroLivre;
+    }
+}
